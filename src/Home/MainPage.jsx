960efafdb8540019c537d82753bb5bf0696101c8
laldinenga <laldinenga11@gmail.com>
2025-03-26 03:32:37 +1100

    change layout

--- src/Home/MainPage.jsx
+++ src/Home/MainPage.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3 } from "react-icons/fa";
+import { FaJava } from "react-icons/fa6";
+import { SiSpringboot } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+import { GrReactjs } from "react-icons/gr";
+import { FaNodeJs } from "react-icons/fa";
+import { SiMysql } from "react-icons/si";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { FaAws } from "react-icons/fa6";
+import { SiTerraform } from "react-icons/si";
+import { FaLinux } from "react-icons/fa6";
+import { VscTerminalBash } from "react-icons/vsc";
+import { FaPython } from "react-icons/fa";
+import { SiVmware } from "react-icons/si";
+import { SiVirtualbox } from "react-icons/si";
+import { BsMicrosoft } from "react-icons/bs";
+import { BiLogoMicrosoftTeams } from "react-icons/bi";
 
 function MainPage() {
   return (
@@ -39,72 +57,110 @@ function MainPage() {
           AWS
         </p>
         <div className="flex flex-row mt-3 space-x-4">
-          <button className="bg-blue-700">Contact Me</button>
-          <button className="">Download CV</button>
-          <button className=""><FaLinkedin size={24}/></button>
-          <button><FaGithub size={24}/></button>
+          <button className="bg-[#14686C] rounded-xl">Contact Me</button>
+          <button className="bg-[#14686C] rounded-xl">Download CV</button>
+          <button className="bg-[#14686C] rounded-xl">
+            <FaLinkedin size={24} />
+          </button>
+          <button className="bg-[#14686C] rounded-xl">
+            <FaGithub size={24} />
+          </button>
         </div>
       </div>
-      <div className="button-group flex border-2 border-gray-500">
-        <button
-          className="flex-1 border-gray-500 hover:bg-gray-100 focus:outline-none"
-          id="contact-button"
-          onclick="setActive('contact')"
-        >
-          Contact Me
-        </button>
-        <button
-          className="flex-1 border-gray-500 hover:bg-gray-100 focus:outline-none"
-          id="download-button"
-          onclick="setActive('download')"
-        >
-          Download CV
-        </button>
-        <button
-          className="flex-1 hover:bg-gray-100 focus:outline-none"
-          id="linkedin-button"
-          onclick="setActive('linkedin')"
-        >
-          LinkedIn
-        </button>
+      <div className="flex items-center justify-center bg-black">
+        <div className="flex flex-row bg-black justify-center border-1 w-fit rounded-3xl items-center">
+          <button className=" rounded-3xl hover:bg-neutral-800">About</button>
+          <button className="rounded-3xl hover:bg-neutral-800">Project</button>
+          <button className="rounded-3xl hover:bg-neutral-800">
+            Education
+          </button>
+        </div>
       </div>
-      <div className="mt-2 text-left bg-gray-500">
-        <h2 className="bg-amber-700 rounded-2xl p-2 w-33">About My Skills</h2>
-        <p>
+      <div className=" bg-black">
+        <div className="pt-16 font-serif">
+          <p>EXPLORE ABOUT ME</p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          {/* vertical line block */}
+          <div class="w-px h-32 bg-gradient-to-b from-black to-[#198072]"></div>
+
+          <div className="w-6 bg-gradient-to-r from-[#2dd4bf] to-[#1f2937] rounded-full">
+            <p className="">1</p>
+          </div>
+          <h2 className="pt-4 text-3xl font-bold bg-gradient-to-r from-[#2dd4bf] to-[#1f2937] bg-clip-text text-transparent w-fit">
+            About
+          </h2>
+          <p className="p-2">
+            I'm Lal Din. <br /> I've got a year of experience in IT Support
+            where I Troubleshot hardware, software and Network issues. <br />{" "}
+            Installation, configuration and maintainence of systems for optimal
+            performance are my expertise. <br /> I was the mastermind when it
+            comes to troubleshooting.
+          </p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          {/* vertical line */}
+          <div class="w-px h-32 bg-gradient-to-b from-black to-[#198072]"></div>
+          <div className="w-6 bg-gradient-to-r from-[#2dd4bf] to-[#1f2937] rounded-full">
+            <p className="">2</p>
+          </div>
+          <h2 className="text-3xl font-bold bg-gradient-to-r from-[#2dd4bf] to-[#1f2937] bg-clip-text text-transparent w-fit">Experience and Hands on Skills</h2>
+          
+
+        </div>
+        {/* <p>
           I am well competent with below technologies and ready to kick start a
           career in any of the areas.
-        </p>
-        <ol className="list-decimal ml-4">
-          <li>Software Development</li>
-          <ul className="list-disc ml-8">
-            <li>Java</li>
-            <li>SpringBoot</li>
-            <li>Javascript</li>
-            <li>ReactJS</li>
-            <li>NodeJS</li>
-            <li>SQL</li>
-          </ul>
-          <li>Cloud Administration</li>
-          <ul className="list-disc ml-8">
-            <li>AWS</li>
-            <li>Linux OS</li>
-            <li>Scripting with Bash and Python</li>
-            <li>AWS Cloudformation</li>
-          </ul>
-          <li>IT Support</li>
-          <ul className="list-disc ml-8">
-            <li>VMWare</li>
-            <li>VM VirtualBox</li>
-            <li>Microsoft 365</li>
-            <li>Active Directory</li>
-            <li>IT Networking</li>
-            <li>etc.</li>
-          </ul>
-        </ol>
+        </p> */}
+        <div className="p-2">
+          <p className="">A year of experience in</p>
+          <ul className="flex flex-row justify-center items-center p-2 space-x-5">
+              <li>Microsoft 365</li>
+              <li>Active Directory</li>
+              <li><SiVirtualbox size={30}/></li>
+              <li><SiVmware size={30}/></li>
+              <li><BsMicrosoft size={30}/></li>
+              <li><BiLogoMicrosoftTeams size={30}/></li>
+              <li>IT Networking</li>
+              <li>etc.</li>
+            </ul>
+        </div>
+        <div className="p-2">
+          <p>Internship Experience in Software Development with</p>
+          <ul className="flex flex-row justify-center items-center p-2 space-x-5">
+              <li><FaJava size={30}/></li>
+              <li><SiSpringboot size={30}/></li>
+              <li><IoLogoJavascript size={30}/></li>
+              <li><GrReactjs size={30}/></li>
+              <li><FaNodeJs size={30}/></li>
+              <li><SiMysql size={30}/></li>
+              <li><BiLogoPostgresql size={30}/></li>
+            </ul>
+        </div>
+        <div className="p-2">
+          <p>Hands on Experience and certifications in</p>
+          <ul className="flex flex-row justify-center items-center p-2 space-x-5">
+              <li><FaAws size={30}/></li>
+              <li><FaLinux size={30}/></li>
+              <li><SiTerraform size={30}/></li>
+              <li><VscTerminalBash size={30}/></li>
+              <li><FaPython size={30}/></li>
+            </ul>
+        </div>
         <div className="">
-          <h1>Project</h1>
-          <div className="grid grid-cols-2 bg-blue-950">
-            <div className="p-2">
+          <div className="flex flex-col justify-center items-center">
+            {/* vertical line */}
+            <div class="w-px h-32 bg-gradient-to-b from-black to-[#198072]"></div>
+            <div className="w-6 bg-gradient-to-r from-[#2dd4bf] to-[#1f2937] rounded-full">
+              <p className="">3</p>
+            </div>
+            <h2 className="text-3xl font-bold bg-gradient-to-r from-[#2dd4bf] to-[#1f2937] 
+            bg-clip-text text-transparent w-fit">Project</h2>
+          </div>
+          {/* <div class="border-r-4 border-blue-500 h-64"></div> */}
+
+          <div className="grid grid-cols-2 bg-black">
+            <div className="p-2 border-r-1 border-blue-500 h-64">
               <img
                 className="w-lg h-64"
                 src="../../public/Image/1.jpg"
@@ -135,7 +191,7 @@ function MainPage() {
               <p>Tech stacks used for this project</p>
               <ul>
                 <li>ReactJS</li>
-                <li>SCSS</li>
+                <li>SASS</li>
                 <li>NodeJS</li>
                 <li>MySql</li>
                 <li>AWS RDS</li>
